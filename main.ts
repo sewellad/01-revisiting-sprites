1a6083a2e4455e4c99d2817833da510daf0ffddb
sewellad <144348593+sewellad@users.noreply.github.com>
2024-01-22 09:03:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+namespace SpriteKind {
+    export const hero = SpriteKind.create()
+    export const nonhero = SpriteKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    non_hero.follow(hero, 50)
+    controller.moveSprite(non_hero)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    non_hero.follow(hero, 50)
+    controller.moveSprite(non_hero)
+})
+sprites.onOverlap(SpriteKind.hero, SpriteKind.nonhero, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    game.gameOver(false)
+})
+let non_hero: Sprite = null
+let hero: Sprite = null
+hero = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.hero)
+controller.moveSprite(hero)
+non_hero = sprites.create(img`
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c 4 4 c c c c f . 
+    . f f c c 4 4 4 4 c c f f . 
+    . f f f b f 4 4 f b f f f . 
+    . f f 4 1 f d d f 1 4 f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e 4 4 4 4 e f e . . 
+    . e 4 f b 3 3 3 3 b f 4 e . 
+    . 4 d f 3 3 3 3 3 3 c d 4 . 
+    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
+    `, SpriteKind.nonhero)
+non_hero.setPosition(18, 18)
